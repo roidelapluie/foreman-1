0d94a7b7c500fe9267734301e93efe041dfa870a
David Blaisonneau <david.blaisonneau@orange.com>
2015-07-27 18:01:47 +0200

    keep 1 README

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ setup(
     author_email='david.blaisonneau@orange.com, arnaud1.morin@orange.com',
     packages=['Foreman'],
     scripts=[],
-    url='http://pypi.python.org/pypi/Foreman/',
+    url='https://github.com/davidblaisonneau-orange/foreman',
+    download_url='https://pypi.python.org/packages/source/F/Foreman/Foreman-0.9.tar.gz',
     license='LICENSE',
     description='Python library to manipulate The Foreman through the API',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     install_requires=['requests-futures', 'requests', 'json'],
+    keywords = ['foreman'],
 )
