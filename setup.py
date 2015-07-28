1bef89a0c8d323600d50e9313252e942387f2d6b
David Blaisonneau <david.blaisonneau@orange.com>
2015-07-28 14:20:51 +0200

    rename module from "Foreman" to "foreman"

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='Foreman',
-    version='0.9.1',
+    name='foreman',
+    packages=['foreman'],
+    version='0.9.4',
     author='David Blaisonneau, Arnaud Morin',
     author_email='david.blaisonneau@orange.com, arnaud1.morin@orange.com',
-    packages=['Foreman'],
     scripts=[],
     url='https://github.com/davidblaisonneau-orange/foreman',
-    download_url='https://pypi.python.org/packages/source/F/Foreman/Foreman-0.9.tar.gz',
-    license='LICENSE',
+    download_url='https://pypi.python.org/packages/source/F/Foreman/Foreman-0.9.3.tar.gz',
+    license=open('LICENSE').read(),
     description='Python library to manipulate The Foreman through the API',
     long_description=open('README.rst').read(),
     install_requires=['requests-futures', 'requests'],
